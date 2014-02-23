7758850d86ad8114ac6123e3d1baae9cda5255bf
Jesus <nomellames@gmail.com>
2014-02-22 17:12:35 -0800

    Added posting to the REST API for bluetooth and 802.11

--- client/runclient.py
+++ client/runclient.py
@@ -8,10 +8,17 @@ from kismetclient import handlers
 from pprint import pprint
 
 import logging
+
+import json
+import requests
+
 log = logging.getLogger('kismetclient')
 log.addHandler(logging.StreamHandler())
 log.setLevel(logging.DEBUG)
 
+endpoint_AP = "http://54.83.8.108:8080/AP"
+endpoint_BT = "http://54.83.8.108:8080/BT"
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 address = ('127.0.0.1', 2501)
 k = KismetClient(address)
@@ -20,8 +27,22 @@ k.register_handler('TRACKINFO', handlers.print_fields)
 
 def handle_ssid(client, ssid, mac):
     print 'ssid spotted: "%s" with mac %s' % (ssid, mac)
+    print(data)
 
-k.register_handler('SSID', handle_ssid)
+    
+
+def handle_bluetooth(client, bdaddr, firsttime, lasttime):
+    print 'Bluetooth device spotted: "%s" First seen %s Last seen %s' % (bdaddr, firsttime, lasttime)
+    data = 'Bluetooth device spotted: "%s" First seen %s Last seen %s' % (bdaddr, firsttime, lasttime)
+    data = {"bdaddr": bdaddr, "LON": "-122.403999", "LAT":"37.777899"}	#data to be send to the server
+    print data
+    r = requests.post(endpoint_BT, data=json.dumps(data), headers=headers)
+
+
+
+
+#k.register_handler('SSID', handle_ssid)
+k.register_handler('BTBBDEV', handle_bluetooth)
 
 try:
     while True:
